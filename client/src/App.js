76389d924eb27a87e1fdd07ca65967334b5a680e
sarath <sarathsarath93366@gmail.com>
2024-01-22 23:22:17 +0530

    first commit client side and server side folder added

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+
+
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+
+      <h1 className=' text-7xl text-center text-blue-700'  > welcome  </h1>
+      
+    </div>
+  )
+}
+
+export default App
