889c4a21a56961449059791a61d5c30cf6203d75
Nithul <mnithul@gmail.com>
2024-01-25 12:02:30 +0530

    home page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Login from './components/Login.jsx'
 import UserDetails from './components/UserDetails.jsx'
+import Home from './components/Home.jsx'
 
 function App() {
   return (
     <div>
+      <Home/>
       <Login/> 
       <UserDetails/>
     </div>
