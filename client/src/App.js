622970f7483b87a647f63c92517ef98fedc1b47e
Nithul <mnithul@gmail.com>
2024-01-26 15:15:39 +0530

    navbar added

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,26 @@ import React from 'react'
 import Login from './components/Login.jsx'
 import UserDetails from './components/UserDetails.jsx'
 import Home from './components/Home.jsx'
+import Header from './components/Header'
+import Footer from "./components/Footer";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import About from './components/About'
+import Contact from './components/Contact'
 
 function App() {
   return (
-    <div>
-      <Home/>
-      <Login/> 
-      <UserDetails/>
-    </div>
+    <Router>
+    <Header />
+      <Routes>
+        
+        <Route path = "/" element={<Home/>}/>
+        <Route path = "/about" element={<About/>}/>
+        <Route path = "/contact" element={<Contact/>}/>
+
+      </Routes>
+      <Footer />
+    </Router>
+
   )
 }
 
