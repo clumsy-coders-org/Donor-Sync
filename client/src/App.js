894c9ecb084c91cf8772eb493481666a69efd703
sarath <sarathsarath93366@gmail.com>
2024-01-30 23:08:17 +0530

    angani login page modifide added

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,7 @@ function App() {
         <Route path = "/about" element={<About/>}/>
         <Route path = "/contact" element={<Contact/>}/>
         <Route path="/login" element={<Login/>}/>
+         
          </Routes>
 
       <Footer />
