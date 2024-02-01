6a40599b6e09e98f2bf8f388e89a84a9315f8b09
ARUN-MOHAN-USHA <126851308+ARUN-MOHAN-USHA@users.noreply.github.com>
2024-02-01 16:13:57 +0530

    Update App.js
    Router updated

--- client/src/App.js
+++ client/src/App.js
@@ -25,6 +25,7 @@ function App() {
         <Route path = "/contact" element={<Contact/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path='/signup' element={<Signup/>} />
+        <Route path='/userdetails' element={<UserDetails/>} />
          
          
        </Routes>
