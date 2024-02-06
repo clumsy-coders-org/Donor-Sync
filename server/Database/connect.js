4e077cacb86ad88092ca901b60fe06348d2c9349
sarath <sarathsarath93366@gmail.com>
2024-02-06 08:24:03 +0530

    server side basic configure ok . DB atlas connected ok

---
+++ server/Database/connect.js
@@ -0,0 +1,24 @@
+
+
+const moongoose=require("mongoose");
+require('dotenv').config()
+
+
+
+
+module.exports.DBConnect=()=>{
+
+    moongoose.connect(process.env.DB_URL).then(()=>{
+
+          console.log("DB connected")
+    }).catch(err=>{
+
+          console.log("DB connecting failed",err)
+    })
+
+       
+}
+
+
+
+
