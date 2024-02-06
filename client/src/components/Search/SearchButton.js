9c74d4e1a8b6ab87868e3e4eb77de0f0e73cec9b
Shamna K <shamnashami500@gmail.com>
2024-02-06 17:53:17 +0530

    Search added

---
+++ client/src/components/Search/SearchButton.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const SearchButton = ({onClick}) => {
+
+    return (
+        <div className="flex items-center justify-center">
+            <button 
+                className="w-64 bg-zinc-100 py-3 pl-2 font-semibold rounded-[10px]" 
+                onClick={onClick}
+            >               
+                Search
+            </button>
+        </div>
+        
+    );
+};
+
+export default SearchButton;
