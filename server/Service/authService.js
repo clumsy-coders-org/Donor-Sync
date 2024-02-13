854cd794a789e9d10d05942a385695b3f136f813
Nithul <mnithul@gmail.com>
2024-02-13 19:50:00 +0530

    ok

--- server/Service/authService.js
+++ server/Service/authService.js
@@ -1,4 +1,63 @@
 
 
 
-console.log("welcome")
+const bcrypt = require("bcrypt")
+const express = require("express")
+module.exports={
+
+    user_signup:(data)=>{                           //user signup Database query
+        return new Promise (async(resolve,reject)=>{
+            const userdata = await usermodel.findOne({email:data.email})
+
+            if(userdata){
+                resolve({exist:true})
+            }else{
+                const salt = await bcrypt.genSalt(10);
+                data.password = await bcrypt.hash(data.password, salt)
+
+                const final = new usermodel(data)
+
+                final.save().then(()=>{
+                    resolve({flag:true})
+                }).catch({err:err})
+
+                }
+            })
+        }
+
+       
+    }    
+
+
+
+    module.exports={
+
+        user_login:(data)=>{                           //user login Database query
+            return new Promise (async(resolve,reject)=>{
+                try{
+                const userdata = await usermodel.findOne({email:data.email})
+    
+                if(!userdata){
+                    resolve({emailerr:true})
+                }else{
+    
+                    const final = await bcrypt.compare(data.password, userdata.password)
+
+                        if(!final){
+                        resolve({flag:false})
+                    }else{
+                        resolve({flag:true, data:userdata})
+                    }
+                }
+                }catch(error){
+                    reject({err:error})
+                }
+            })
+    
+          }
+        }    
+
+    
+
+
+
