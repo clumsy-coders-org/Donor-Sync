11cf2d8c8ee52216f3ab76f81e0f7e0c143513a9
sarath <sarathsarath93366@gmail.com>
2024-02-16 13:03:53 +0530

    search page design ok

--- client/src/components/Search/FilterComponent.jsx
+++ client/src/components/Search/FilterComponent.jsx
@@ -4,10 +4,110 @@
 
 import React, { useState } from 'react'
 import { FaSearch } from "react-icons/fa";
+import { BsFillPersonFill } from "react-icons/bs";
+import { FaPhone } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+import ReactPaginate from "react-paginate"
+import "./styile.css"
 
 const FilterComponent = () => {
 
     const [arrya, setarrya] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    const [show,setshow]=useState(true)
+    const [spinner,setspinner]=useState(true)
+    // const [donoe,setdonor]=useState(true)
+    const [donor,setdonor]=useState(true)
+
+
+  // pagination code start
+
+    const [pageNumber, setPageNumber] = useState(0);
+
+    const userPrePage = 10;
+    const pageVisited = pageNumber * userPrePage;
+
+    const pageCount = Math.ceil(arrya.length / userPrePage);
+
+    const changePage = ({ selected }) => {
+
+        setPageNumber(selected);
+
+    }
+
+    const displyaData = arrya.slice(pageVisited, pageVisited + userPrePage)
+        .map((obj, index) =>
+
+        (
+
+            donor ?
+
+
+            <div className='w-[250px] h-[160px] rounded-3xl bg-gray-300 shadow-xl pl-8    '  >
+
+                <div className='flex'  > < BsFillPersonFill className='text-blue-800 text-[18px] mr-3  ' /> <span> krishna kumar </span>  </div>
+
+                <div className='flex mt-2'  > <  FaPhone className='text-blue-800 text-[18px] mr-3  ' />    <span> 7592831937 </span>  </div>
+
+
+                <div className='flex mt-2'  > <span className='text-blue-800 mr-3 '  > Blood Group :  </span>   <span> O+ </span>  </div>
+
+               <div className='flex mt-2'  > < MdEmail className='text-blue-800 text-[18px] mr-3 ' />    <span> sarath@gmail.com </span>  </div>
+
+               <div className='flex mt-2'  > <span className='text-blue-800 mr-3 '  > Unit :  </span>   <span> 4 </span>  </div>
+
+               
+               </div>
+
+
+
+
+
+
+            :
+
+
+
+
+
+            <div className='w-[250px] h-[150px] rounded-3xl bg-gray-300 shadow-xl pl-8 pt-6   '  >
+
+                <div className='flex'  > < BsFillPersonFill className='text-blue-800 text-[18px] mr-3  ' /> <span> krishna kumar </span>  </div>
+
+                <div className='flex mt-2'  > <  FaPhone className='text-blue-800 text-[18px] mr-3  ' />    <span> 7592831937 </span>  </div>
+
+
+                <div className='flex mt-2'  > <span className='text-blue-800 mr-3 '  > Blood Group :  </span>   <span> O+ </span>  </div>
+
+                <div className='flex mt-2'  > < MdEmail className='text-blue-800 text-[18px] mr-3 ' />    <span> sarath@gmail.com </span>  </div>
+
+               </div>
+
+
+
+        ))
+
+         // pagination code end
+
+
+
+         const formsubmit=()=>{
+
+                 setshow(false)
+
+                 setTimeout(()=>{
+
+                    setspinner(false)
+
+                 },3000)
+         }
+
+
+
+
+
+
+
+
 
 
     return (
@@ -33,35 +133,103 @@ const FilterComponent = () => {
                     <option value="">  City </option>
                 </select>
 
-                <button className='w-[100px] h-[30px] bg-red-700 text-white flex justify-center mt-4 sm:mt-0  items-center rounded-xl'> <FaSearch /> </button>
+                <button onClick={formsubmit}  className='w-[150px] h-[40px] bg-red-700 text-white flex justify-center mt-4 sm:mt-0  items-center rounded-xl'> <FaSearch /> </button>
 
 
             </div>
 
 
-            <div className=' container mx-auto h-[600px]  bg-green-700 flex flex-wrap gap-5 justify-center pt-7  '>
+            <div className=' w-full  min-h-[400px] mt-24  sm:mt-0 flex flex-wrap gap-5 justify-center ' >
 
-                
-                    {
+                {
 
-                        arrya.map((obj) => (
+                    show ?
+                    
+                    <div className='w-full h-[400px] pl-8  flex justify-center items-center' >
 
-                            <div className='w-[250px] h-[150px] bg-gray-500 rounded-3xl  '>
-
-
-                            </div>
-
-                        ))
-                    }
+                        <h1 className='font-bold text-[25px]' > Find Your Donor or Blood Bank </h1>
 
 
-              
+                    </div>
+                    
 
+                    :
+
+                   spinner ?
+
+                   <div className='w-full h-[400px] pl-8  flex justify-center items-center' >
+
+                   <h1 className='font-bold text-[25px]' > spinner </h1>
+
+
+               </div>
+
+               :
+
+                   
+
+                    displyaData
+               
+               
+               
+               }
 
 
 
 
             </div>
+
+            <div className='mt-8 sm:mt-0  mb-10 flex justify-center ' >
+
+                {
+
+                     show ?
+
+                     null
+
+                     :
+
+
+                     spinner ?
+
+                     null
+
+                     :
+
+                     <ReactPaginate
+
+                     previousLabel={"previous"}
+ 
+                     nextLabel={"next"}
+ 
+                     pageCount={pageCount}
+ 
+                     onPageChange={changePage}
+ 
+                     containerClassName={"paginationBttns"}
+                     pageLinkClassName={"previousBttn"}
+                     nextLinkClassName={"nextBttn"}
+                     disabledClassName={"paginationDisabled"}
+                     activeClassName={"paginationActive"}
+                 />
+ 
+
+
+                }
+
+
+               
+
+
+
+            </div>
+
+
+
+
+
+
+
 
 
 
