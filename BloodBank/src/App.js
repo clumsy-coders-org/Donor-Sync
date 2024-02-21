4fd635ac06c2e84f7ef2ace627730eea297d384a
Nithul <mnithul@gmail.com>
2024-02-21 18:50:52 +0530

    login and register for bloodbank  added

--- BloodBank/src/App.js
+++ BloodBank/src/App.js
@@ -3,7 +3,7 @@
 import React from 'react'
 import {Route,Routes} from "react-router-dom"
 import Login from './components/Login/Login.jsx'
-import Register from './Components/Register/Register.jsx'
+import Register from './components/Register/Register.jsx'
 import Home from './components/home/Home.jsx'
 
 
@@ -13,7 +13,7 @@ function App() {
     <div>
        <Routes>
        <Route path="/" element={<Home/>}/>
-       
+
 
           <Route path="/login" element={<Login/>}/>
           <Route path='/signup' element={<Register/>} />
