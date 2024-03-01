f111ce090e066777314971fc5d4e6fb2957ea5d1
sarath <sarathsarath93366@gmail.com>
2024-03-01 08:08:18 +0530

    blood bank deploy time err 2

--- BloodBank/src/App.js
+++ BloodBank/src/App.js
@@ -20,9 +20,9 @@ function App(){
       
       <Routes>
         <Route path="/" element={<Welcome />} />
-        <Route path='/home' element={<Home />} />
+        {/* <Route path='/home' element={<Home />} /> */}
         {/* <Route path="/login" element={<Login />} /> */}
-        <Route path='/signup' element={<Register />} />
+        {/* <Route path='/signup' element={<Register />} /> */}
 
       </Routes>
 
