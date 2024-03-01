b75effa137196f238c5bd95ecb60ee8fee6ff5d0
sarath <sarathsarath93366@gmail.com>
2024-03-01 08:04:46 +0530

    blood bank deploy time err

--- BloodBank/src/App.js
+++ BloodBank/src/App.js
@@ -3,7 +3,7 @@
 import React from 'react'
 
 import { Route, Routes } from "react-router-dom"
-import Login from './components/Login/Login.jsx'
+// import Login from './components/Login/Login.jsx'
 import Register from './components/Register/Register.jsx'
 import Home from './components/home/Home.jsx'
 import Welcome from './components/welcomee/welcome.jsx'
@@ -21,7 +21,7 @@ function App(){
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path='/home' element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/login" element={<Login />} /> */}
         <Route path='/signup' element={<Register />} />
 
       </Routes>
