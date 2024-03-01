03ba05313ec2985c70934cfc18fbfb31d2f6d820
sarath <sarathsarath93366@gmail.com>
2024-03-01 10:00:43 +0530

    client side navigation changes ok

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -154,7 +154,7 @@ function Header() {
               <Disclosure.Button
                 key={item.name}
                 as="a"
-                href={item.href}
+                onClick={()=>{navigate(item.href)}}
                 className={classNames(
                   item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                   'block rounded-md px-3 py-2 text-base font-medium'
